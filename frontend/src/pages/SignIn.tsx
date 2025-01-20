157a8cae9e50840b4bf3dc0aa57cec1257e201ea
Abdul Mannan <mannan.jnv123@gmail.com>
2025-01-20 19:39:17 +0530

    finalized

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -2,7 +2,7 @@ import { Auth } from "../components/Auth";
 import { Qoute } from "../components/Qoute";
 export const SignIn = () => {
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-1 lg:grid-cols-2">
       <div className="flex justify-center items-center text-xl font-bold">
         <Auth type="signin" />
       </div>
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -2,7 +2,7 @@ import { Qoute } from "../components/Qoute";
 import { Auth } from "../components/Auth";
 export const SignUp = () => {
   return (
-    <div className="grid grid-cols-1 no-scrollbar lg:grid-cols-2">
+    <div className="grid grid-cols-1 lg:grid-cols-2">
       <div className="flex justify-center items-center text-xl font-bold">
         <Auth type="signup" />
       </div>
